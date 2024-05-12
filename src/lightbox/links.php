dfb52e2b494f9ac80bcb633b193ef72d00efb66f
eliemr <77899370+eliemr@users.noreply.github.com>
2024-05-12 03:12:29 +0300

    Add files via upload

---
+++ src/lightbox/links.php
@@ -0,0 +1,6 @@
+<?php
+  $links = array(
+    'css' => 'src/lightbox/css/lightbox.min.css',
+    'js' => 'src/lightbox/js/lightbox.min.js'
+  );
+?>
---
+++ src/waypoints/links.php
@@ -0,0 +1,5 @@
+<?php
+  $links = array(
+    'js' => 'src/waypoints/waypoints.min.js'
+  );
+?>
